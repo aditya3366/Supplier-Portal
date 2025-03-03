e26b69819514722fbb52ba01013aab6ce10a99b7
Aditya Das <adityadas3366@gmail.com>
2025-03-03 16:11:30 +0530

    Add:ASN Doctype Added

---
+++ supplier_portal/supplier_portal/doctype/advance_shipment_notification/advance_shipment_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aditya Das and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Advance Shipment Notification", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ supplier_portal/supplier_portal/doctype/advance_shipment_notification/test_advance_shipment_notification.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aditya Das and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAdvanceShipmentNotification(FrappeTestCase):
+	pass
