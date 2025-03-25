c26eefbf4eff3f076762c63351706e284df21588
Aditya Das <adityadas3366@gmail.com>
2025-03-25 15:29:07 +0530

    Fix:link url

--- supplier_portal/supplier_portal/doctype/advance_shipment_notification/advance_shipment_notification.js
+++ supplier_portal/supplier_portal/doctype/advance_shipment_notification/advance_shipment_notification.js
@@ -6,3 +6,24 @@
 
 // 	},
 // });
+frappe.ui.form.on("Advance Shipment Notification", {
+    refresh: function (frm) {
+        // Add button only if the ASN is submitted
+        if (frm.doc.name) {
+            frm.add_custom_button(__('Create Purchase Receipt'), function () {
+                frappe.call({
+                    method: "supplier_portal.supplier_portal.doctype.advance_shipment_notification.advance_shipment_notification.create_purchase_receipt",
+                    args: {
+                        asn_name: frm.doc.name
+                    },
+                    callback: function (r) {
+                        if (r.message) {
+                            frappe.msgprint(__("Purchase Receipt {0} created successfully!", [r.message]));
+                            frappe.set_route("Form", "Purchase Receipt", r.message);
+                        }
+                    }
+                });
+            }, __("Create"));
+        }
+    }
+});
--- supplier_portal/supplier_portal/doctype/advance_shipment_notification/advance_shipment_notification.py
+++ supplier_portal/supplier_portal/doctype/advance_shipment_notification/advance_shipment_notification.py
@@ -7,3 +7,35 @@ from frappe.model.document import Document
 
 class AdvanceShipmentNotification(Document):
 	pass
+
+import frappe
+from frappe.model.document import Document
+
+@frappe.whitelist()
+def create_purchase_receipt(asn_name):
+    asn = frappe.get_doc("Advance Shipping Notification", asn_name)
+
+    # Create new Purchase Receipt
+    pr = frappe.new_doc("Purchase Receipt")
+    pr.supplier = asn.supplier  # Map supplier
+    pr.supplier_invoice_no = asn.supplier_invoice_no
+    pr.invoice_date = asn.invoice_date
+    pr.shipping_by = asn.shipping_by
+    pr.vehicle_no = asn.vehical_no
+    pr.asn_reference = asn.name  # Add ASN reference for tracking
+
+    # Add Items from ASN
+    for item in asn.items:
+        pr.append("items", {
+            "item_code": item.item_code,
+            "item_name": item.item_name,
+            "qty": item.supplied_quantity,
+            "uom": item.uom,
+            "purchase_order": item.purchase_order
+            
+        })
+
+    pr.insert(ignore_permissions=True)  # Insert as draft
+    frappe.msgprint(_("Purchase Receipt {0} created successfully!".format(pr.name)))
+
+    return pr.name
